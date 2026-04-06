8ba98b4734e9a34ed03309151a43f322c2560a62
Rocco Georgi <mail@roccogeorgi.com>
2026-04-06 12:31:47 +0200

    after-course update, release 2026-01-30

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -3,4 +3,4 @@ rsync -avr --delete-before  \
                          --exclude '.*'  \
                          --exclude '_deploy-dev.sh' \
                          --exclude '_deploy-prod.sh' \
-/Users/Rocco/Develop/pavingways/wonderdots.ch/dist/* root@164.90.218.79:/var/www/skatepark/www/
+./dist/ root@164.90.218.79:/var/www/skatepark/www/
--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -2,9 +2,9 @@ npm run build
 rsync -avr --delete-before  \
   --include=".nojekyll" \
   --exclude=".*" \
-  /Users/Rocco/Develop/pavingways/wonderdots.ch/dist/ /Users/Rocco/Develop/pavingways/wonderdots.github.io/
-cp .gitignore /Users/Rocco/Develop/pavingways/wonderdots.github.io/
-cd /Users/Rocco/Develop/pavingways/wonderdots.github.io/
+  ./dist/ ../wonderdots.github.io/
+cp .gitignore ../wonderdots.github.io/
+cd ../wonderdots.github.io/
 git add .
 git commit -am "release $(date +'%Y-%m-%d')"
 git push origin main
