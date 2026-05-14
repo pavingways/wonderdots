d957f3e35c05ef0bec5182982441d51331545812
Rocco Georgi <mail@roccogeorgi.com>
2026-05-14 23:57:33 +0200

    really removed baseUrl now

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,46 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// Ambient module declarations for project path aliases (no baseUrl in tsconfig)
+declare module "@lib/*" {
+	const value: any;
+	export default value;
+}
+
+declare module "@utils/*" {
+	const value: any;
+	export default value;
+}
+
+declare module "@components/*" {
+	const value: any;
+	export default value;
+}
+
+declare module "@layouts/*" {
+	const value: any;
+	export default value;
+}
+
+declare module "@assets/*" {
+	const value: any;
+	export default value;
+}
+
+declare module "@pages/*" {
+	const value: any;
+	export default value;
+}
+
+// Allow bare 'assets/...' imports
+declare module "assets/*" {
+	const value: any;
+	export default value;
+}
+
+// Generic static asset types
+declare module "*.svg" {
+	const content: any;
+	export default content;
+}
+
